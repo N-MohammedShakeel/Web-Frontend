a0a5befbfae5e443c883fb1b5fbbfcfc6f025025
Mohammed Shakeel <nmohammedshakeel22@gmail.com>
2024-12-15 22:06:41 +0530

    updated

---
+++ jquery/index.js
@@ -0,0 +1,17 @@
+// $("button").click(function (e) { 
+//    $("h1").css("color", "purple");
+   
+// });
+
+// $("body").keydown(function(e) { 
+//     $("h1").text(e.key);
+//     console.log(e.key)
+// });
+
+// $("h1").mouseleave(function () { 
+//     $("h1").css("color", "black");
+// });
+
+// $("h1").mouseenter(function () { 
+//     $("h1").css("color", "purple");
+// });
